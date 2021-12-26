ebf6049951c8e62f820eb27dfff1f846a98f747b
KriZ SeR <92368721+KANNANSIR@users.noreply.github.com>
2021-12-26 16:59:01 +0530

    Update sc.js

--- plugins/sc.js
+++ plugins/sc.js
@@ -1,7 +1,7 @@
 let handler  = async (m, { itsu, usedPrefix: _p }) => {
 prep = itsu.prepareMessageFromContent(m.chat, { orderMessage: { 
 itemCount: 999999999999, status: 1,
-message: 'this bot just recode of script \n\nhttps://www.instagram.com/kannanbro_f_f',
+message: 'INSTAYIL FOLLOW CHEY MWONU\n\nhttps://www.instagram.com/kannanbro_f_f',
 orderTitle: 'B',
 sellerJid: '0@s.whatsapp.net',
 thumbnail: global.thumb
