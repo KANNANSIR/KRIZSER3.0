8175c30f905e9a6e988d01481ab63c2eb976b5e2
KriZ SeR <92368721+KANNANSIR@users.noreply.github.com>
2021-12-26 17:55:41 +0530

    Update _autodownload.js

--- plugins/_autodownload.js
+++ plugins/_autodownload.js
@@ -1,73 +1,84 @@
-const { igdl, twitter, pin } = require('../lib/scrape')
-const { ytIdRegex, servers, yta, ytv } = require('../lib/y2mate')
+const { igdl, tiktok, twitter, pin } = require('../lib/scrape')
+const { facebook } = require('../lib/facebook')
+const { servers, yta, ytv } = require('../lib/y2mate')
 const fetch = require('node-fetch')
-
+let yts = require('yt-search')
+let util = require('util')
 let handler = m => m
 
-handler.before = async function (m, { isPrems }) {
-    let chat = db.data.chats[m.chat]
-    let user = db.data.users[m.sender]
-    let set = db.data.settings[this.user.jid]
+handler.all = async function (m, { isPrems }) {
+
     if (m.chat.endsWith('broadcast')) return
-    if (chat.isBanned || user.banned || !chat.download || m.isBaileys) return
+    if (db.data.users[m.sender].banned) return
+    if (db.data.chats[m.chat].isBanned) return
 
-    if (/https?:\/\/(www\.|v(t|m)\.|t\.)?tiktok\.com/i.test(m.text)) {
-        let res = await fetch(API('amel', '/tiktok', { url: m.text.match(/https?:\/\/(www\.|v(t|m)\.|t\.)?tiktok\.com\/.*/i)[0].split(/\n| /i)[0] }, 'apikey'))
-        if (!res.ok) return m.reply(eror)
-        let json = await res.json()
-        if (!json.status) return m.reply(this.format(json))
-        await m.reply(wait)
-        await this.sendFile(m.chat, json.videoSD, '', '© KRIZSER', m)
-    }
+    let url = m.text.split(/\n| /i)[0]
 
-    if (/https?:\/\/i\.coco\.fun\//i.test(m.text)) {
-        let res = await fetch(API('jojo', '/api/cocofun-no-wm', { url: m.text.match(/https?:\/\/i\.coco\.fun\/.*/i)[0].split(/\n| /i)[0] }))
+    if (/^.*tiktok/i.test(m.text)) {
+        let res = await fetch(API('hardianto', '/api/download/tiktok', { url }, 'apikey'))
         if (!res.ok) return m.reply(eror)
         let json = await res.json()
         await m.reply(wait)
-        await this.sendFile(m.chat, json.download, '', '© KRIZSER', m)
+        // m.reply(util.format(json))
+        await this.sendFile(m.chat, json.nowm, '', footer, m)
     }
 
-    if (/https?:\/\/(fb\.watch|(www\.|web\.|m\.)?facebook\.com)/i.test(m.text)) {
-        let res = await fetch(API('neoxr', '/api/download/fb', { url: m.text.match(/https?:\/\/(fb\.watch|(www\.|web\.|m\.)?facebook\.com)\/.*/i)[0].split(/\n| /i)[0] }, 'apikey'))
+    if (/^.*cocofun/i.test(m.text)) {
+        let res = await fetch(API('jojo', '/api/cocofun-no-wm', { url }))
         if (!res.ok) return m.reply(eror)
         let json = await res.json()
-        if (!json.status) return m.reply(this.format(json))
         await m.reply(wait)
-        await conn.sendFile(m.chat, json.data.sd.url, '', `HD: ${json.data.hd.url}\nUkuran: ${json.data.hd.size}\n\n© stikerin`, m)
+        // m.reply(util.format(json))
+        await this.sendFile(m.chat, json.download, '', footer, m)
     }
 
-    if (/https?:\/\/(www\.)?instagram\.com\/(p|reel|tv)/i.test(m.text)) {
-        igdl(m.text.match(/https?:\/\/(www\.)?instagram\.com\/(p|reel|tv)\/.*/i)[0].split(/\n| /i)[0]).then(async res => {
-            let json = JSON.parse(JSON.stringify(res))
+    if (/^.*(fb.watch|facebook.com)/i.test(m.text)) {
+        let res = await fetch(API('neoxr', '/api/download/fb', { url }, 'apikey'))
+        if (!res.ok) return m.reply(eror)
+        let json = await res.json()
+        if (!json.status) return m.reply(util.format(json))
+        await m.reply(wait)
+        await conn.sendFile(m.chat, json.data.sd.url, '', `HD: ${json.data.hd.url}\nUkuran: ${json.data.hd.size}\n\n`, m)
+    }
+
+    if (/^.*instagram.com\/(p|reel|tv)/i.test(m.text)) {
+        igdl(url).then(async res => {
+            let igdl = JSON.stringify(res)
+            let json = JSON.parse(igdl)
             await m.reply(wait)
             for (let { downloadUrl, type } of json) {
-                this.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), '© stikerin', m)
+                this.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), footer, m, 0, { thumbnail: await (await fetch(downloadUrl)).buffer() })
             }
         }).catch(_ => _)
     }
 
-    if (/https?:\/\/(www\.)?(pinterest\.com\/pin|pin\.it)/i.test(m.text)) {
-        pin(m.text.match(/https?:\/\/(www\.)?(pinterest\.com\/pin|pin\.it).*/i)[0].split(/\n| /i)[0]).then(async res => {
-            let json = JSON.parse(JSON.stringify(res))
+    if (/^.*(pinterest.com\/pin|pin.it)/i.test(m.text)) {
+        pin(url).then(async res => {
+            let pin = JSON.stringify(res)
+            let json = JSON.parse(pin)
             if (!json.status) return m.reply(eror)
             await m.reply(wait)
-            this.sendFile(m.chat, json.data[0].url, json.data[0].url, '© stikerin', m)
+            m.reply(util.format(json))
+            await this.sendFile(m.chat, json.data.url, '', footer, m)
         }).catch(_ => _)
     }
 
-    if (/https?:\/\/(www\.)?twitter\.com\/.*\/status/i.test(m.text)) {
-        twitter(m.text.match(/https?:\/\/(www\.)?twitter\.com\/.*\/status\/.*/i)[0].split(/\n| /i)[0]).then(async res => {
-            let json = JSON.parse(JSON.stringify(res))
+    if (/^.*twitter.com\//i.test(m.text)) {
+        twitter(url).then(async res => {
+            let twit = JSON.stringify(res)
+            let json = JSON.parse(twit)
             let pesan = json.data.map((v) => `Link: ${v.url}`).join('\n------------\n')
             await m.reply(wait)
             for (let { url } of json.data) {
-                this.sendFile(m.chat, url, 'tw' + (/mp4/i.test(url) ? '.mp4' : '.jpg'), '© stikerin', m)
+                this.sendFile(m.chat, url, 'ig' + (/mp4/i.test(url) ? '.mp4' : '.jpg'), footer, m)
             }
         }).catch(_ => _)
     }
 
-    if (ytIdRegex.test(m.text) || ytIdRegex.test(m.selectedButtonId)) {
+    if (/^https?:\/\/.*youtu/i.test(m.text)) {
+        let results = await yts(url)
+        let vid = results.all.find(video => video.seconds < 3600)
+        if (!vid) return m.reply('Video/Audio Tidak ditemukan')
         let yt = false
         let usedServer = servers[0]
         for (let i in servers) {
@@ -78,21 +89,21 @@ handler.before = async function (m, { isPrems }) {
                 usedServer = server
                 break
             } catch (e) {
-                // m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
+                m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
             }
         }
         if (yt === false) return m.reply(eror)
         if (yt2 === false) return m.reply(eror)
-        let { thumb, title, filesizeF } = yt
-        await this.send2ButtonLoc(m.chat, thumb, `
+        let { dl_link, thumb, title, filesize, filesizeF } = yt
+        await this.send2ButtonLoc(m.chat, await (await fetch(thumb)).buffer(), `
 *Judul:* ${title}
 *Ukuran File Audio:* ${filesizeF}
 *Ukuran File Video:* ${yt2.filesizeF}
 *Server y2mate:* ${usedServer}
-`.trim(), '© KRIZSER', 'Audio', `.yta ${vid.url}`, 'Video', `.ytv ${vid.url}`)
+`.trim(), footer, 'Audio', `.yta ${vid.url}`, 'Video', `.yt ${vid.url}`)
     }
 
-    return !0
 }
 
+handler.limit = true
 module.exports = handler
