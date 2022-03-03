25d804dd82135598fdcbd10f5f9b13f087b5e045
KriZ SeR <92368721+KANNANSIR@users.noreply.github.com>
2022-03-03 13:51:52 +0530

    Update mode.js

--- plugins/mode.js
+++ plugins/mode.js
@@ -2,7 +2,7 @@ let handler = async(m, { conn, usedPrefix, command }) => {
   let isPublic = command === "public";
   let self = global.opts["self"]
 
-  if(self === !isPublic) return conn.sendButton(m.chat, `Eva Is ${!isPublic ? "Self" : "Public"} from earlier ${m.sender.split("@")[0] === global.owner[0] ? "Mbak" : "Owner"} :v`, wm, false, [[`${!isPublic ? "Public" : "Self"}`, `${usedPrefix}${!isPublic ? "public" : "self"}`]], m)
+  if(self === !isPublic) return conn.sendButton(m.chat, `KriZSeR Is ${!isPublic ? "Self" : "Public"} from earlier ${m.sender.split("@")[0] === global.owner[0] ? "Mbak" : "Owner"} :v`, wm, false, [[`${!isPublic ? "Public" : "Self"}`, `${usedPrefix}${!isPublic ? "public" : "self"}`]], m)
 
   global.opts["self"] = !isPublic
 
